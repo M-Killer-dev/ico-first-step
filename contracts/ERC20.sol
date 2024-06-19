88cf08b0efb3e1c39279d058b7172619e5f75e48
M-Killer-dev <mykhailokoroldev@outlook.com>
2024-06-18 21:39:30 -0400

    feat: add IERC20Metadata interface to ERC20 contract

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -28,3 +28,20 @@ interface IERC20 {
     //sends the 'amount' tokens 'from' account to 'to' instead of owner, then the`amount` is then deducted from the caller's allowance.
     function transferFrom(address from, address to, uint256 amount) external returns (bool);
 }
+
+interface IERC20Metadata is IERC20 {
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() external view returns (string memory);
+
+    /**
+     * @dev Returns the symbol of the token.
+     */
+    function symbol() external view returns (string memory);
+
+    /**
+     * @dev Returns the decimals places of the token.
+     */
+    function decimals() external view returns (uint8);
+}
