d01e81110030b00f14eef31e579b7b4ffc074652
M-Killer-dev <mykhailokoroldev@outlook.com>
2024-06-22 22:28:47 -0400

    feat: add whitelist filter in buytokensfunction in TokenSale contrac t

--- contracts/TokenSale.sol
+++ contracts/TokenSale.sol
@@ -11,13 +11,24 @@ contract TokenSale {
     address public wallet;
     ERC20 public token;
 
+    mapping(address => bool) public contractsWhiteList;
+
+    modifier onlyWhitelisted() {
+        if (!whitelist()) {
+            _;
+        } else {
+            require(contractsWhiteList[msg.sender], "You are not whitelisted");
+            _;
+        }
+    }
+
     constructor(uint256 _rate, address _wallet, ERC20 _token) public {
         rate = _rate;
         wallet = _wallet;
         token = _token;
     }
 
-    function buyTokens(address _beneficiary) public payable returns (bool) {
+    function buyTokens(address _beneficiary) public payable onlyWhitelisted whenNotPaused returns (bool) {
         require(_beneficiary != address(0));
         require(msg.value != 0);
 
@@ -25,6 +36,6 @@ contract TokenSale {
         weiRased = weiRased + msg.value;
 
         token.transfer(_beneficiary, tokens);
-        payable (wallet).transfer(msg.value);
+        payable(wallet).transfer(msg.value);
     }
 }
