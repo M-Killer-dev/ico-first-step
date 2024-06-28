6d0401a9e05b1d883c0c0981c65949956cbb893e
M-Killer-dev <mykhailokoroldev@outlook.com>
2024-06-28 11:51:54 -0400

    feat: modify the migration file and add TokenSaleTest file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,7 @@ const SimpleToken = artifacts.require("SimpleToken");
 const TokenSale = artifacts.require("TokenSale");
 
 module.exports = function (deployer) {
-  //   deployer.deploy(SimpleToken, "SimpleToken", "STK").then(function (simpleToken) {
-  //     return deployer.deploy(TokenSale, 4000, simpleToken, 500000000000000000000);
-  //   });
-  deployer.deploy(SimpleToken, "SimpleToken", "STK");
+    deployer.deploy(SimpleToken, "SimpleToken", "STK").then(function (simpleToken) {
+      return deployer.deploy(TokenSale, 4000, simpleToken.address, 5000000000000000);
+    });
 };
---
+++ test/TokenSaleTest.js
@@ -0,0 +1,7 @@
+var TokenSale = artifacts.require("./TokenSale.sol");
+
+contract("TokenSale", function (accounts) {
+  var tokenInstance;
+
+  it("initializes the contract with the correct values", function () {});
+});
