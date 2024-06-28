bcf6b45c5a4e8f8c37e19f283793f82efb6029f8
M-Killer-dev <mykhailokoroldev@outlook.com>
2024-06-27 22:41:29 -0400

    feat: add SimpleTokenTest.js for test

--- test/SimpleTokenTest.js
+++ test/SimpleTokenTest.js
@@ -1,18 +1,52 @@
 var SimpleToken = artifacts.require("./SimpleToken.sol");
 
-contract('SimpleToken', function(accounts) {
+contract("SimpleToken", function (accounts) {
   var tokenInstance;
 
-  it('initializes the contract with the correct values', function() {
-    console.log("here")
-    // return MyToken.deployed().then(function(instance) {
-    //   tokenInstance = instance;
-    //   return tokenInstance.name();
-    // }).then(function(name) {
-    //   assert.equal(name, 'MyToken', 'has the correct name');
-    //   return tokenInstance.symbol();
-    // }).then(function(symbol) {
-    //   assert.equal(symbol, 'MT', 'has the correct symbol');
-    // })
-  })
+  it("initializes the contract with the correct values", function () {
+    return SimpleToken.deployed()
+      .then((instance) => {
+        tokenInstance = instance;
+        return tokenInstance.name();
+      })
+      .then((name) => {
+        assert.equal(name, "SimpleToken", "has the correct name");
+        return tokenInstance.symbol();
+      })
+      .then((symbol) => {
+        assert.equal(symbol, "STK", "has the correct symbol");
+        return tokenInstance.balanceOf(accounts[0]);
+      })
+      .then((balance0) => {
+        assert.equal(
+          balance0.toString(),
+          "100000000000000000000000000000",
+          "failed mint in constructor"
+        );
+        return tokenInstance.burn(1000, { from: accounts[0] });
+      })
+      .then((sender) => {
+        assert.equal(!sender, false, "error when burn");
+        return tokenInstance.balance();
+      })
+      .then((balance) => {
+        assert.equal(!balance, false, "error when balance");
+        return tokenInstance.setPresaleContractAddress({ from: accounts[1] });
+      })
+      .then((presaleContractAddress) => {
+        assert.equal(
+          !presaleContractAddress,
+          false,
+          "error when setPresaleContractAddress"
+        );
+        return tokenInstance.approvePresaleContract(1000, {from: accounts[1]});
+      })
+      .then((status) => {
+        // assert.equal(status, true, "rror when approvePresaleContract");
+        return tokenInstance.getPresaleContractAddress();
+      })
+      .then((presaleContractAddress) => {
+        assert.equal(!presaleContractAddress, false, "error when balance");
+      });
+  });
 });
