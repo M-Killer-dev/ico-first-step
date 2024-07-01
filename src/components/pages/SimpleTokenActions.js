5a2fa02ccd35c1639ca2a2e1a5fdd2067b20c6be
M-Killer-dev <mykhailokoroldev@outlook.com>
2024-07-01 16:58:29 -0400

    feat: add approve function in SimpleTokenActions

--- src/components/pages/SimpleTokenActions.js
+++ src/components/pages/SimpleTokenActions.js
@@ -4,6 +4,8 @@ export default function SimpleTokenActions({ account, accounts, simpleToken }) {
   const [burnAmount, setBurnAmount] = useState(0);
   const [transferAmount, setTransferAmount] = useState(0);
   const [receiver, setReciver] = useState();
+  const [approveAmount, setApproveAmount] = useState(0);
+  const [spender, setSpender] = useState("");
   const [sender, setSender] = useState();
   const [receiver1, setReciver1] = useState();
   const [availableAmount, setAvailableAmount] = useState(0);
@@ -38,6 +40,36 @@ export default function SimpleTokenActions({ account, accounts, simpleToken }) {
       try {
         await simpleToken.methods
           .transfer(receiver, web3.utils.toBigInt(transferAmount))
+          .send({ from: account });
+      } catch (error) {
+        console.error("An error occurred:", error);
+      }
+    }
+  };
+
+  const handleChangeApproveAmount = (e) => {
+    setApproveAmount(e.target.value);
+  };
+
+  const handleSpenderChange = async (e) => {
+    setSpender(e.target.value);
+    if (simpleToken && account) {
+      try {
+        let amount = await simpleToken.methods
+          .allowance(account, e.target.value)
+          .call();
+        setApproveAmount(amount.toString());
+      } catch (error) {
+        console.error("An error occurred:", error);
+      }
+    }
+  };
+
+  const handleApprove = async () => {
+    if (simpleToken && account) {
+      try {
+        await simpleToken.methods
+          .approve(spender, web3.utils.toBigInt(approveAmount))
           .send({ from: account });
       } catch (error) {
         console.error("An error occurred:", error);
@@ -100,6 +132,25 @@ export default function SimpleTokenActions({ account, accounts, simpleToken }) {
           Transfer
         </button>
       </div>
+      <div className="approve d-flex">
+        <h3>Approve</h3>
+        <input
+          type="text"
+          value={approveAmount}
+          onChange={handleChangeApproveAmount}
+        ></input>
+        <select name="spender" onChange={handleSpenderChange}>
+          <option value="null"></option>
+          {accounts.map((item, index) => (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+        <button className="btn btn-primary" onClick={handleApprove}>
+          Approve
+        </button>
+      </div>
       <div className="transfer-from d-flex">
         <input
           type="text"
@@ -114,7 +165,7 @@ export default function SimpleTokenActions({ account, accounts, simpleToken }) {
             </option>
           ))}
         </select>
-        <select name="receiver" onChange={handleReceiver1Change}>
+        <select name="receiver1" onChange={handleReceiver1Change}>
           <option value="null"></option>
           {accounts.map((item, index) => (
             <option key={item} value={item}>
